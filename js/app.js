45ea2ec0767659fff86e813db076027f0ceb9fa6
ChestnutC <Christopher.Chestnut@dowjones.com>
2015-09-15 13:23:05 -0400

    slim nav

--- js/app.js
+++ js/app.js
@@ -1,31 +1,67 @@
 import React from 'react';
 import Router from 'react-router';
+import ExecutionEnv from 'react/lib/ExecutionEnvironment';
 import { DefaultRoute, Link, Route, RouteHandler } from 'react-router';
 import stylesheet from '../css/components/mainPage.scss';
 import headerSheet from '../css/components/header.scss';
 require('../css/fonts.scss');
 
-import LoginHandler from './components/Login.js';
 import MainHandler from './components/Page.js';
+import {TrainingHandler, ShadowHandler, VideoHandler, DevHandler}
+  from './components/pageHandlers.js';
 
+const WINDOW_CONSTANT = 154;
 let App = React.createClass({
+  getInitialState() {
+    return (window.scrollY > WINDOW_CONSTANT) ?
+      {navSize: 'slim'} : {navSize: 'regular'};
+  },
+
   render() {
+    let classString = 'nav at8units ' + this.state.navSize;
     return (
-      <div className="nav at8units">
+      <div className={classString}>
         <div className="NavHeader">
           <Link to="main">Main</Link>
-          <Link to="login">Login</Link>
+          <Link to="shadow">Shadow Boxing</Link>
         </div>
         {/* this is the important part */}
         <RouteHandler/>
       </div>
     );
+  },
+
+  componentDidMount() {
+    if (ExecutionEnv.canUseDOM) {
+      window.addEventListener('scroll', this.handleScroll);
+    }
+  },
+
+  componentWillUnMount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+
+  handleScroll() {
+    console.log(window.scrollY);
+    if(window.scrollY > 103) {
+      this.setState(
+        {
+          navSize: 'slim'
+        }
+      );
+    } else {
+      this.setState(
+        {
+          navSize: 'regular'
+        }
+      );
+    }
   }
 });
 
 let routes = (
   <Route name="app" path="/main" handler={App}>
-    <Route name="login" path="/login" handler={LoginHandler}/>
+    <Route name="shadow" path="/shadow" handler={ShadowHandler}/>
     <Route name="main" path="/" handler={MainHandler}/>
   </Route>
 );
---
+++ js/components/pageHandlers.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export let ShadowHandler = React.createClass({
+  displayName: 'ShadowPage',
+  render(){
+    return(
+      <div/>
+    );
+  }
+});
+
+/*export let TrainingHandler = ShadowHandler,
+  VideoHandler = ShadowHandler,
+  DevHandler = ShadowHandler;*/
+
